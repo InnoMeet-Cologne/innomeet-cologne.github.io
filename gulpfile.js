63554d1f913d13843b0de4e932a2da16898d5483
Alwin Mark <a.mark@crowdfox.com>
2018-06-14 15:38:19 +0200

    first style changes

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var connect = require('gulp-connect');
+var pkg = require('./package.json');
+
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function() {
+
+  // Bootstrap
+  gulp.src([
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+    .pipe(gulp.dest('./vendor/bootstrap'))
+
+  // jQuery
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'))
+
+})
+
+gulp.task('webserver', function() {
+  connect.server();
+});
+
+// Default task
+gulp.task('default', ['vendor', 'webserver']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Dev task
+gulp.task('dev', ['browserSync'], function() {
+  gulp.watch('./css/*.css', browserSync.reload);
+  gulp.watch('./*.html', browserSync.reload);
+});
